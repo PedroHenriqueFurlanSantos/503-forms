3148fe3e5a50ede02d3bca14a0523eb233640d2a
Pedro <Furlansantos812@gmail.com>
2019-08-01 15:45:07 -0300

    me matei e nao fluiu

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-let 
+let nome = document.querySelector(".nome");
+let email = document.querySelector(".email");
+let confirm = document.querySelector(".confirm");
+let naoSou = document.querySelector("a");
+let botao = document.querySelector("button");
+let texto = document.querySelector("p");
+
+localStorage.setItem('email','w@gmail.com');
+
+if(localStorage.email == confirm){
+    form.style.display="none";
+    section.style.display="initial";
+    texto.innerHTML=`Seja bem vindo ${localStorage.email}`;
+    naoSou.innerHTML=`Não sou ${localStorage.email};`
+}
+else{
+    function acessar(){
+        form.style.display="initial";
+        localStorage.setItem("email", email.value);
+        if(localStorage.nome){
+            div.style.display="none";
+            section.style.display="initial";
+            texto.innerHTML=`Seja bem vindo ${localStorage.email}`;
+            naoSou.innerHTML=`Não sou ${localStorage.email};`
+        }
+    }
+}
+
+function limpar(){
+    localStorage.removeItem('email');
+}
+
+botao.onclick = acessar;
+naoSou.onclick = limpar;
